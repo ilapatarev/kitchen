29d61d4ebb6e883d4a8100a4944cfc09d6aa99e9
ilapatarev <106161445+ilapatarev@users.noreply.github.com>
2024-05-12 18:05:12 +0300

    Merge remote-tracking branch 'origin/master'

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^iepwnq6cddte$o_4(9d(g%b82_&&9d9z0-(nm1xf-u-+cqcj_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
