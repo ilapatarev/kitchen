2ee1aa18b3ef113a3c565a47caa66d249f792cb6
ilapatarev <106161445+ilapatarev@users.noreply.github.com>
2024-06-15 17:36:33 +0300

    Merge remote-tracking branch 'origin/master'

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from kitchen import accounts
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 import environ
 
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
